b3f473ed2e2e037abea817af0d1f20d19cb3e2fa
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2022-03-10 13:24:33 -0500

    git status

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,9 +2,7 @@ const os = require("os");
 
 export default function handler(req, res) {
   res.status(200).json({
-    client: req.headers["user-agent"],
-    server: `${os.arch()} ${os.platform()} ${os.release()} ${os.hostname()} ${
-      os.version
-    }`,
+    client: req.headers,
+    server: os.networkInterfaces(),
   });
 }
